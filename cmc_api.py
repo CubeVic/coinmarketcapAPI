266755454cf23ea1980254e181748200c3fe46ff
Booker <victoraguirre.f@gmail.com>
2022-04-28 21:38:46 +0800

    [20220418] MODIFY: Modify the cmc_api file to make it a class

--- cmc_api.py
+++ cmc_api.py
@@ -6,8 +6,13 @@ import time
 from datetime import datetime
 import logging
 
-def _configure_cmc_logger():
-	global cmc_logger
+
+class CmcApi:
+	BASE_URL = 'https://pro-api.coinmarketcap.com'
+	SANDBOX_URL = 'https://sandbox-api.coinmarketcap.com'
+
+	timestamp = str(datetime.utcfromtimestamp(time.time()))[0:10].replace("-", "_")
+
 	cmc_logger = logging.getLogger(__name__)
 	cmc_logger.setLevel(logging.DEBUG)
 
@@ -23,114 +28,113 @@ def _configure_cmc_logger():
 	cmc_logger.addHandler(file_handler)
 	cmc_logger.addHandler(stream_handler)
 
-
-
-class CmcApi(CMC_API):
-
-	def __init__(self):
-		_configure_cmc_logger()
-
-
-def cnc_configuration():
-	_configure_cmc_logger()
-	global CNC_API_KEY
-	global BASE_URL
-	global SANDBOX_URL
-	global HEADERS
-	global cmc_logger
-	CNC_API_KEY = os.environ['COIN_API_KEY']
-	BASE_URL = 'https://pro-api.coinmarketcap.com'
-	SANDBOX_URL = 'https://sandbox-api.coinmarketcap.com'
-	HEADERS = {
-		'Accepts': 'application/json',
-		'X-CMC_PRO_API_KEY': CNC_API_KEY,
-	}
-	cmc_logger.info(f'Starting script')
-
-
-def prepare_session() -> Session:
-	request_session = Session()
-	request_session.headers.update(HEADERS)
-	return request_session
-
-
-def get_time() -> datetime:
-	t = datetime.utcfromtimestamp(time.time())
-	return t
-
-
-def get_updated_prices_from_cmc() -> dict:
-	""" Query the APi for the latest prices, it consumes credits.
-
-	:param: request_session
-	:return: json
-	"""
-	request_session = prepare_session()
-	timestamp = str(get_time())[0:10].replace("-", "_")
-	if os.path.exists(f"price-{timestamp}.json"):
-		cmc_logger.info(f'JSON File already exist: price-{timestamp}.json')
-		data = get_price_from_json_file(timestamp)
-		cmc_logger.debug(f"Date obtained from Json file\n{data}")
-		return {
-			'timestamp': data['timestamp'],
-			'data': data['data']
+	def __init__(self, cmc_api, is_sandbox_url):
+		self.cmc_logger.info(f'Starting CMC')
+		self._cmc_api = cmc_api
+		self.HEADERS = {
+			'Accepts': 'application/json',
+			'X-CMC_PRO_API_KEY': self._cmc_api,
 		}
-	else:
-		params = {
-			'start': '1',
-			'limit': '1000',
-			'convert': 'USD',
-		}
-		endpoint = '/v1/cryptocurrency/listings/latest'
-		url = BASE_URL + endpoint
-		# url = SANDBOX_URL + endpoint
-		try:
-			response = request_session.get(url, params=params)
-		except (ConnectionError, Timeout, TooManyRedirects) as e:
-			cmc_logger.error(f"error with the connection to CMC API\n{e}")
+		self._is_sandbox_url = is_sandbox_url
+		self.uri = ""
+		self.request_session = Session()
+		self.request_session.headers.update(self.HEADERS)
+
+	@property
+	def cmc_api(self):
+		return "key"
+
+	@cmc_api.setter
+	def cmc_api(self, cmc_api):
+		self.cmc_logger.info('Adding key')
+		self._cmc_api = cmc_api
+
+	@cmc_api.deleter
+	def cmc_api(self):
+		del self.cmc_api
+
+	@property
+	def is_sandbox_url(self):
+		return self._is_sandbox_url
+
+	@is_sandbox_url.setter
+	def is_sandbox_url(self, is_sandbox_url):
+		if is_sandbox_url:
+			self.cmc_logger.info(f'Setting the Sandbox URL {self.SANDBOX_URL}')
+			self.uri = self.SANDBOX_URL
 		else:
-			data = json.loads(response.text)
-			cmc_logger.info(f'Error Message: {data["status"]["error_message"]}, '
-			                f'Error code: {data["status"]["error_code"]},'
-			                f'status: {data["status"]["timestamp"]}')
+			self.cmc_logger.info(f'Setting the URL {self.BASE_URL}')
+			self.uri = self.BASE_URL
 
-			timestamp = data['status']['timestamp'].replace("-", "_").replace(":", "_")
-			timestamp = timestamp[0:10]
+	def get_updated_prices_from_cmc(self) -> dict:
+		""" Query the APi for the latest prices, it consumes credits.
 
-			result = {
-				'timestamp': data['status']['timestamp'],
-				'data': data['data']
-			}
+		:param: request_session
+		:return: json
+		"""
 
-			with open(f'price-{timestamp}.json', 'w') as file:
-				json.dump(result, file, indent=6)
-				cmc_logger.info(f'File created: price-{timestamp}.json')
-
+		if os.path.exists(f"price-{self.timestamp}.json"):
+			self.cmc_logger.info(f'JSON File already exist: price-{self.timestamp}.json')
+			data = self._get_price_from_json_file(self.timestamp)
+			self.cmc_logger.debug(f"Date obtained from Json file\n{data}")
 			return {
-				'timestamp': data['status']['timestamp'],
+				'timestamp': data['timestamp'],
 				'data': data['data']
 			}
+		else:
 
+			params = {
+				'start': '1',
+				'limit': '1000',
+				'convert': 'USD',
+			}
+			endpoint = '/v1/cryptocurrency/listings/latest'
+			url = self.SANDBOX_URL + endpoint if self.is_sandbox_url else self.BASE_URL + endpoint
+			try:
+				response = self.request_session.get(url, params=params)
+			except (ConnectionError, Timeout, TooManyRedirects) as e:
+				self.cmc_logger.error(f"error with the connection to CMC API\n{e}")
+			else:
+				data = json.loads(response.text)
+				self.cmc_logger.info(f'Error Message: {data["status"]["error_message"]}, '
+									 f'Error code: {data["status"]["error_code"]},'
+									 f'status: {data["status"]["timestamp"]}')
 
-def get_map():
-	request_session = prepare_session()
-	if os.path.exists(f"map.json"):
-		cmc_logger.info('File map already exist')
-	else:
-		endpoint = '/v1/cryptocurrency/map'
-		url = BASE_URL + endpoint
-		params = {
-			'sort': 'cmc_rank',
-		}
-		map_resp = request_session.get(url=url, params=params)
-		cmc_map = json.loads(map_resp.text)
-		with open('map.json', 'w') as file:
-			json.dump(cmc_map['data'], file, indent=6)
-			cmc_logger.info(f'creating map.json file')
+				timestamp = data['status']['timestamp'].replace("-", "_").replace(":", "_")
+				timestamp = timestamp[0:10]
 
+				result = {
+					'timestamp': data['status']['timestamp'],
+					'data': data['data']
+				}
 
-def get_price_from_json_file(timestamp: str) -> dict:
-	with open(f'price-{timestamp}.json', 'r') as file:
-		payload = json.loads(file.read())
-	return payload
+				with open(f'price-{timestamp}.json', 'w') as file:
+					json.dump(result, file, indent=6)
+					self.cmc_logger.info(f'File created: price-{timestamp}.json')
 
+				return {
+					'timestamp': data['status']['timestamp'],
+					'data': data['data']
+				}
+
+	def get_map(self):
+
+		if os.path.exists(f"map.json"):
+			self.cmc_logger.info('File map already exist')
+		else:
+			endpoint = '/v1/cryptocurrency/map'
+			url = self.BASE_URL + endpoint
+			params = {
+				'sort': 'cmc_rank',
+			}
+			map_resp = self.request_session.get(url=url, params=params)
+			cmc_map = json.loads(map_resp.text)
+			with open('map.json', 'w') as file:
+				json.dump(cmc_map['data'], file, indent=6)
+				self.cmc_logger.info(f'creating map.json file')
+
+	@staticmethod
+	def _get_price_from_json_file(timestamp: str) -> dict:
+		with open(f'price-{timestamp}.json', 'r') as file:
+			payload = json.loads(file.read())
+		return payload
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import cmc_api
 from models import Prices
+import os
 
 if __name__ == '__main__':
 	db = Prices()
-	cmc_api.cnc_configuration()
-	data = cmc_api.get_updated_prices_from_cmc()
+	# data = cmc_api.get_updated_prices_from_cmc()
 	# print(db.read_all())
+	cmc = cmc_api.CmcApi(os.environ['CMC_API'], is_sandbox_url=False)
+	cmc.get_updated_prices_from_cmc()
+	cmc.get_map()
+
