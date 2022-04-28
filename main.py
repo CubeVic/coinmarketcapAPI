30e66948623c6e17658d8485aa6e059774b9d62b
Booker <victoraguirre.f@gmail.com>
2022-04-28 23:46:47 +0800

    [20220418] UPDATING: Updating class models to add select last update

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import os
 
 if __name__ == '__main__':
 	db = Prices()
-	# data = cmc_api.get_updated_prices_from_cmc()
-	# print(db.read_all())
 	cmc = cmc_api.CmcApi(os.environ['CMC_API'], is_sandbox_url=False)
+
 	cmc.get_updated_prices_from_cmc()
 	cmc.get_map()
 
+	print(db.select_last_updated())
